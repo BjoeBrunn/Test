095962ce68c37fbc5ab6c32dabe6db93523cea31
Björn <bjoernbrunnermeier@hotmail.com>
2013-04-09 12:13:37 +0200

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Björn on 09.04.13.
+//  Copyright (c) 2013 Björn Brunnermeier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Björn on 09.04.13.
+//  Copyright (c) 2013 Björn Brunnermeier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
